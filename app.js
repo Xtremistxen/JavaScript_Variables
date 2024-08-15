48d3e82eed481b4fe71059b865e9b0abf9f8a6e8
Robert Montgomery <boobyvxvbooby_gmail.com>
2024-08-15 11:27:09 -0500

    We left a well constructed language to CHAOS! either embrace or cower!

--- app.js
+++ app.js
@@ -2,9 +2,53 @@ console.log("Hello World!\n==========\n");
 console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
+// console.log   This will log
 
 // Exercise 1
+const firstName; // This will seem to generate an error with exercise2, possibly intended.
+
+let lastName;
+
+var age;
 
 // Exercise 2
+firstName = "Robert"; // conflicts will exercise1, it suggests to change to "let"
+
+lastName = "Montgomery";
+
+age = 35;
 
 // Exercise 3
+let language = "JavaScript";
+
+let createdYear = 1995;
+
+let isCaseSensitive = true;
+
+// Exercise 4
+let price = 19.99;
+
+let isOnSale = false;
+
+let salePercentage = 15;
+
+let stock = 0;
+
+let inStock = false;
+
+let selectSize = "M";
+
+// Exercise 5
+let title = "Name of the Wind";
+
+let author = "Patrick Rothfuss";
+
+let pageCount = 722;
+
+let bookmark = 456;
+
+let hasRead = true;
+
+
+
+
